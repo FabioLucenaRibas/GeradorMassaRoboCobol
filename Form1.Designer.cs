7307991ed94486a3befc4a1a3303795c70840922
Fábio Lucena Ribas <fabio.luce@outlook.com>
2019-02-18 21:25:55 -0300

    Commit inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GeradorMassaRobo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.checkBoxGeraXML = new MaterialSkin.Controls.MaterialCheckBox();
+            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.txtOpenFile = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.materialTabSelector1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabSelector1.Location = new System.Drawing.Point(0, 0);
+            this.materialTabSelector1.Margin = new System.Windows.Forms.Padding(2);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(745, 41);
+            this.materialTabSelector1.TabIndex = 0;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.materialTabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(745, 116);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.checkBoxGeraXML);
+            this.tabPage1.Controls.Add(this.materialRaisedButton2);
+            this.tabPage1.Controls.Add(this.materialRaisedButton1);
+            this.tabPage1.Controls.Add(this.txtOpenFile);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(737, 90);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Gerador - massa - ROBO  | COBOL";
+            // 
+            // checkBoxGeraXML
+            // 
+            this.checkBoxGeraXML.AutoSize = true;
+            this.checkBoxGeraXML.Depth = 0;
+            this.checkBoxGeraXML.Enabled = false;
+            this.checkBoxGeraXML.Font = new System.Drawing.Font("Roboto", 10F);
+            this.checkBoxGeraXML.Location = new System.Drawing.Point(5, 60);
+            this.checkBoxGeraXML.Margin = new System.Windows.Forms.Padding(0);
+            this.checkBoxGeraXML.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.checkBoxGeraXML.MouseState = MaterialSkin.MouseState.HOVER;
+            this.checkBoxGeraXML.Name = "checkBoxGeraXML";
+            this.checkBoxGeraXML.Ripple = true;
+            this.checkBoxGeraXML.Size = new System.Drawing.Size(90, 30);
+            this.checkBoxGeraXML.TabIndex = 4;
+            this.checkBoxGeraXML.Text = "Gera XML";
+            this.checkBoxGeraXML.UseVisualStyleBackColor = true;
+            // 
+            // materialRaisedButton2
+            // 
+            this.materialRaisedButton2.Depth = 0;
+            this.materialRaisedButton2.Location = new System.Drawing.Point(590, 50);
+            this.materialRaisedButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton2.Name = "materialRaisedButton2";
+            this.materialRaisedButton2.Primary = true;
+            this.materialRaisedButton2.Size = new System.Drawing.Size(145, 37);
+            this.materialRaisedButton2.TabIndex = 3;
+            this.materialRaisedButton2.Text = "PROCESSAR";
+            this.materialRaisedButton2.UseVisualStyleBackColor = true;
+            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(590, 8);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(145, 37);
+            this.materialRaisedButton1.TabIndex = 1;
+            this.materialRaisedButton1.Text = "Abrir arquivo";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // txtOpenFile
+            // 
+            this.txtOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOpenFile.Depth = 0;
+            this.txtOpenFile.Enabled = false;
+            this.txtOpenFile.Hint = "";
+            this.txtOpenFile.Location = new System.Drawing.Point(6, 22);
+            this.txtOpenFile.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOpenFile.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtOpenFile.Name = "txtOpenFile";
+            this.txtOpenFile.PasswordChar = '\0';
+            this.txtOpenFile.SelectedText = "";
+            this.txtOpenFile.SelectionLength = 0;
+            this.txtOpenFile.SelectionStart = 0;
+            this.txtOpenFile.Size = new System.Drawing.Size(569, 23);
+            this.txtOpenFile.TabIndex = 0;
+            this.txtOpenFile.UseSystemPasswordChar = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.materialTabControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 41);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(745, 116);
+            this.panel2.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 157);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gerador de massa - Robo";
+            this.panel1.ResumeLayout(false);
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel2;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private MaterialSkin.Controls.MaterialCheckBox checkBoxGeraXML;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GeradorMassaRobo
+{
+    public partial class Form1 : Form
+    {
+
+        bool json;
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            inicializarOpenFile();
+
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                txtOpenFile.Text = openFileDialog1.FileName;
+                try
+                {
+                    LerEConverterJsonEmXML();
+                }
+                catch(Exception Ex)
+                {
+                    txtOpenFile.Text = string.Empty;
+                    MessageBox.Show(Ex.Message, "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                AtualizarBotaoGerarXML();
+            }
+            else
+            {
+                inicializarOpenFile();
+            }
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(openFileDialog1.FileName)){
+                    MessageBox.Show("Favor informar um arquivo", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    inicializarOpenFile();
+                }
+                else
+                {
+                    XmlDocument xml = LerEConverterJsonEmXML();
+                    String conteudoXML = xml.InnerXml;
+                    formatarSaidaArquivo(conteudoXML, 52);
+
+                    if (checkBoxGeraXML.Checked)
+                    {
+                        geraArquivoXML(xml);
+                    }
+
+                    MessageBox.Show("Processo concluído com sucesso", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    inicializarOpenFile();
+                }
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                inicializarOpenFile();
+            }
+        }
+
+        private string RemoverAcentuacao(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private XmlDocument LerEConverterJsonEmXML()
+        {
+            string conteudo = string.Empty;
+            try
+            {
+                StreamReader x;
+                string Caminho = openFileDialog1.FileName;
+                x = File.OpenText(Caminho);
+                Console.OpenStandardOutput();
+                while (x.EndOfStream != true)
+                {
+                    String conteudoLinha = x.ReadLine();
+                    conteudo += conteudoLinha.Trim();
+                }
+                conteudo = RemoverAcentuacao(conteudo);
+                conteudo = Regex.Replace(conteudo, "[^\\][^0-9A-Za-z-</>'\",-.{}()+:; ]", "");
+                x.Close();
+            }
+            catch
+            {
+                throw new Exception("Ocorreu um erro na leitura do arquivo");
+            }
+                try
+                {
+                    XmlDocument retorno = (XmlDocument)JsonConvert.DeserializeXmlNode(conteudo, "root");
+                    json = true;
+                    return retorno;
+                }
+                catch
+                {
+                    try
+                    {
+
+                        XmlDocument retorno = new XmlDocument();
+                        retorno.LoadXml(conteudo);
+                        json = false;
+                        return retorno;
+                    }
+                    catch
+                    {
+                        throw new Exception("Favor informar um arquivo no formato XML ou JSON");
+                    }
+                }
+        }
+
+        private void geraArquivoXML(XmlDocument xml)
+        {
+            String caminho = openFileDialog1.FileName;
+            String nomeArquivo = openFileDialog1.SafeFileName.Split('.')[0];
+            caminho = caminho.Replace(openFileDialog1.SafeFileName, "");
+            string CaminhoNome = caminho + "XML - " + nomeArquivo + ".xml";
+            xml.Save(CaminhoNome);
+        }
+
+        public void formatarSaidaArquivo(String entrada,
+        int quantidadeCaracteres)
+        {
+            try
+            {
+                StreamWriter arquivo;
+                String caminho = openFileDialog1.FileName;
+                String nomeArquivo = openFileDialog1.SafeFileName.Split('.')[0];
+                caminho = caminho.Replace(openFileDialog1.SafeFileName, "");
+                string CaminhoNome = caminho + "FORMATADO - " + nomeArquivo + ".txt";
+                arquivo = File.CreateText(CaminhoNome);
+
+                if (!string.Empty.Equals(entrada))
+                {
+
+                    var split = entrada.Select((c, index) => new { c, index })
+                        .GroupBy(x => x.index / quantidadeCaracteres)
+                        .Select(group => group.Select(elem => elem.c))
+                        .Select(chars => new string(chars.ToArray()));
+
+                   arquivo.WriteLine("          05 WRK-BLOCO-MENSAGEM.                                        ");
+                    int cont = 0;
+                    foreach (var str in split)
+                    {
+                        cont++;
+                        String espaco = "      ";
+                        String contTamanho = Convert.ToString(cont);
+                        int tamanho = 6;
+                        tamanho = tamanho - contTamanho.Length;
+                        espaco = espaco.Substring(0, tamanho);
+
+                        arquivo.WriteLine("             10  WRK-MENSAGEM-P" + cont + espaco + "PIC X(00" + RetornaTamanhoVariavel(str.Length) + ") VALUE                  ");
+                        arquivo.WriteLine("             '" + str + "'.    ");
+                    }
+                }
+                arquivo.Close();
+            }
+            catch
+            {
+                throw new Exception("Ocorreu um erro ao processar esta ação");
+            }
+        }
+        private string RetornaTamanhoVariavel(int valor)
+        {
+            {
+                String tamanho = Convert.ToString(valor);
+
+                if (tamanho.Length < 2)
+                {
+                    return "0" + tamanho.Length;
+                }
+                return tamanho;
+            }
+        }
+
+        private void rb_Json_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxGeraXML.Visible = true;
+        }
+
+        private void rb_Xml_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxGeraXML.Visible = false;
+        }
+
+        private void inicializarOpenFile()
+        {
+            openFileDialog1 = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "Selecionar Arquivo",
+                InitialDirectory = @"C:\",
+                Filter = "Todos os arquivos (*.*)|*.*|Documentos de texto (*.txt;*.json;*.xml)|*.txt;*.TXT;*.json;*.Json;*.xml;*.XML",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            txtOpenFile.Text = string.Empty;
+            json = false;
+            AtualizarBotaoGerarXML();
+        }
+
+
+        private void AtualizarBotaoGerarXML()
+        {
+            checkBoxGeraXML.Enabled = json;
+            checkBoxGeraXML.Checked = json;
+        }
+    }
+}
